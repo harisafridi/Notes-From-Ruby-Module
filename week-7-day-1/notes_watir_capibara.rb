6f657213319e16ddf46f75ac50cb61f36f8a6d1f
Haris Mohammad <hmohammad@spartaglobal.com>
2015-11-23 16:35:55 +0000

    notes on week 7 day 1

---
+++ week-7-day-1/notes_watir_capibara.rb
@@ -0,0 +1,19 @@
+
+
+Three Kinds of Automated Web Testing
+
+- Web Form DSL e.g. Capybara
+- Browser API e.g. Watir-WebDriver
+- Web Driver API e.g. Selenium-WebDriver
+
+1st Phase: Set Up
+2nd Phase: Execute
+3rd Phase: Verification
+3rd Phase: Tear Down
+
+Watir 
+- Browser API for Selenium-WebDriver
+- Nice DSL
+- PageObject available  //gem...how do we make our own PageObject
+- Built for Ruby
+- Industry Standard
