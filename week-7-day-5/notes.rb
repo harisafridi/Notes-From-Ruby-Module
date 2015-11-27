d033864d680e281dd85b022ce6586c12d457798e
Haris Mohammad <hmohammad@spartaglobal.com>
2015-11-27 17:10:06 +0000

    added week 7 day 5

---
+++ week-7-day-5/notes.rb
@@ -0,0 +1,41 @@
+
+- How to behave in a business environment
+- CV, Personal branding and Interview skills
+- Presentation skills
+- Manual Testing
+- Web, modile, and Web performance Testing
+- Usability Testing and Stress Testing
+- Database design and SQL
+- Agil principles, Scrum and Kanban
+- User Stories and acceptance criteria
+- TDD and Continuous Integration
+- Agile Testing
+
+
+- £40 - 50,000
+- An in-depth understanding  of the Ruby language
+- An intimate knowledge of Watir
+- A good knowledge of Selenium and Capybara
+- Document Object Model
+- Learn fast and efficiently without direction.
+
+- Know exactly how things work
+- Make a PowerPoint to try to solidify things in your head
+- Work on a personal project: make something interesting: must be in Ruby, Flat Finder: narrowed them down and put them into table, split them into tab, spin up a website in sinatra, make a blog, program a robot in Ruby
+ 			4 Day Project, Build a Ruby Gem, add a method or functionality to exisitng Gem: maybe Watir
+
+- All commit on master branch/ feature branching workflow
+- TaskBoard
+- Virual TaskBoard
+- Trello TaskBoard
+- Must use Kanban
+- Use excel sheet if taking too long
+- On friday must be completed: dont have to be in suit, set up a room, recommendintation, deliever documentation: defects log, how to run tests
+Demo individual projects each individual...
+
+NAtinal Rail Test Suit 3: Use
+- work together as a group
+- use each others strength
+- Review each-other's work
+- Build something you are proud of
+
